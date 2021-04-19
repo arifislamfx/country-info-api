4e206a2ba7d27a8ceb864e75afa7f28ef5b0f811
Ariful Islam <iariful.info@gmail.com>
2021-04-19 20:37:20 +0600

    add handle button and cart page

--- src/App.js
+++ src/App.js
@@ -1,41 +1,47 @@
 
-import { useEffect, useState } from 'react';
-import './App.css';
-import Country from './components/Country/Country';
+    import { useEffect, useState } from 'react';
+    import './App.css';
+import Cart from './components/Cart/Cart';
+    import Country from './components/Country/Country';
 
 
-function App() {
+    function App() {
 
-  const [countries, setCountries] = useState([]);
+      const [countries, setCountries] = useState([]);
+      const [cart, setCart] = useState([]);
 
-  useEffect(() => {
-    fetch('https://restcountries.eu/rest/v2/all')
-    .then(res => res.json())
-    .then(data => {
-      setCountries(data);
-    })
-    .catch(err => {
-      console.log(err.message);
-    })
-  }, [])
+      useEffect(() => {
+        fetch('https://restcountries.eu/rest/v2/all')
+        .then(res => res.json())
+        .then(data => {
+          setCountries(data);
+        })
+        .catch(err => {
+          console.log(err.message);
+        })
+      }, [])
 
-  const handleAdd = (country) => {
-    console.log('add cliked', country)
-  }
-
-  return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    <h1>Total country: {countries.length} </h1>
-
-    <ul>
-      {
-        countries.map(country => <Country country = {country} handleAdd = {handleAdd} key = {country.alpha3Code}> </Country>)
+      const handleAdd = (country) => {
+        const newCountry = [...cart, country];
+            setCart(newCountry);
       }
-    </ul>
 
-    </div>
-  );
-}
+      return (
+        <div className="App">
+          <h1>Hello world!</h1>
+        <h1>Total country: {countries.length} </h1>
+        <h3>Country added: {cart.length} </h3>
 
-export default App;
+        <Cart cart = { cart } />
+
+        <ul>
+          {
+            countries.map(country => <Country country = {country} handleAdd = {handleAdd} key = {country.alpha3Code}> </Country>)
+          }
+        </ul>
+
+        </div>
+      );
+    }
+
+    export default App;
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,18 +1,21 @@
 
 
 const Country = (props) => {
-            const {name, capital, population, flag, alpha3Code} = props.country;
+            const {name, capital, population, flag, alpha3Code, region} = props.country;
             
             const handleAdd = props.handleAdd;
-    return (
-        <div style = {{border: "3px solid red", maxWidth: "300px", marginTop: "50px", marginLeft: "200px", padding: "20px 0" }}>
 
-            <h2> Unique code: {alpha3Code} </h2>
+    return (
+        <div style = {{maxWidth: "200px", display: "flex", flexDirection: "row", margin: "0 auto"}}>
+            <div style = {{border: "3px solid red", margin: "10px", padding: "10px" }} >
+            <p> Unique code: {alpha3Code} </p>
             <h3>  {name} </h3>
             <h4>Capital is {capital} </h4>
             <h5>population: {population} </h5>
-            <img src= {flag} width = "100" height = "100" alt="flag"/> <br/>
+            <p>region : {region} </p>
+            <img src= {flag} width = "50" height = "50" alt="flag"/> <br/>
             <button onClick={() => handleAdd(props.country)}>Add Country</button>
+            </div>
         </div>
     );
 };
